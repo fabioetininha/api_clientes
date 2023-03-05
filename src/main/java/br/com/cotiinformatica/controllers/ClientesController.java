16083b6d6b3a16d12a3e2d69367430b050a3d191
fabioetininha <fabioetininha@gmail.com>
2023-03-05 16:02:03 -0300

    Serviços, BeanValidations e conclusão da API

--- src/main/java/br/com/cotiinformatica/controllers/ClientesController.java
+++ src/main/java/br/com/cotiinformatica/controllers/ClientesController.java
@@ -2,7 +2,10 @@ package br.com.cotiinformatica.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,83 +28,157 @@ import io.swagger.annotations.ApiOperation;
 
 @Api(tags = "Clientes")
 @RestController
-
 public class ClientesController {
 
-	//injeção de dependência
+
+	// injeção de dependência
 	@Autowired
 	private ClienteService clienteService;
-	
+
 	@ApiOperation("Serviço para cadastro de clientes.")
 	@PostMapping("/api/clientes")
-	public ResponseEntity<ResponseClientesDTO> post
-(@RequestBody PostClientesDTO dto) {
-			
-		ResponseClientesDTO response = new ResponseClientesDTO();
-		
-		try {
-		
-			ModelMapper modelMapper = new ModelMapper();
-			Cliente cliente = modelMapper.map
-(dto, Cliente.class);
-			
-			clienteService.cadastrar(cliente);
-			
-			response.setStatus(201);
-			response.setMensagem
-("Cliente cadastrado com sucesso.");
+	public ResponseEntity<ResponseClientesDTO> 
+post(@Valid @RequestBody PostClientesDTO dto) {
 
+		ResponseClientesDTO response = new ResponseClientesDTO();
+
+		try {
+
+			ModelMapper modelMapper = new ModelMapper();
+			Cliente cliente = modelMapper.map(dto, Cliente.class);
+
+			clienteService.cadastrar(cliente);
+
+			response.setStatus(201);
+			response.setMensagem("Cliente cadastrado com sucesso.");
 			response.setCliente(modelMapper.map
 (cliente, GetClientesDTO.class));
-			
-			return ResponseEntity.status(HttpStatus.CREATED)
-.body(response);
-		}
-		catch(IllegalArgumentException e) {
-			
+
+			return ResponseEntity.status(HttpStatus.CREATED).body(response);
+		} catch (IllegalArgumentException e) {
+
 			response.setStatus(400);
 			response.setMensagem(e.getMessage());
-			
+
 			return ResponseEntity.status
 (HttpStatus.BAD_REQUEST).body(response);
-		}
-		catch(Exception e) {
-			
+		} catch (Exception e) {
+
 			response.setStatus(500);
 			response.setMensagem(e.getMessage());
-			
+
 			return ResponseEntity.status
 (HttpStatus.INTERNAL_SERVER_ERROR).body(response);
-		}		
+		}
 	}
-
-
-
 
 	@ApiOperation("Serviço para edição de clientes.")
 	@PutMapping("/api/clientes")
-	public ResponseEntity<GetClientesDTO> put
-(@RequestBody PutClientesDTO dto) {
-		return null;
+	public ResponseEntity<ResponseClientesDTO> put
+(@Valid @RequestBody PutClientesDTO dto) {
+
+		ResponseClientesDTO response = new ResponseClientesDTO();
+
+		try {
+
+			ModelMapper modelMapper = new ModelMapper();
+			Cliente cliente = modelMapper.map(dto, Cliente.class);
+			
+			clienteService.atualizar(cliente);
+			
+			response.setStatus(200);
+			response.setMensagem("Cliente atualizado com sucesso.");
+			response.setCliente
+(modelMapper.map(cliente, GetClientesDTO.class));
+
+			return ResponseEntity.status(HttpStatus.OK).body(response);
+			
+		} catch (IllegalArgumentException e) {
+
+			response.setStatus(400);
+			response.setMensagem(e.getMessage());
+
+			return ResponseEntity.status
+(HttpStatus.BAD_REQUEST).body(response);
+			
+		} catch (Exception e) {
+
+			response.setStatus(500);
+			response.setMensagem(e.getMessage());
+
+			return ResponseEntity.status
+(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+		}
 	}
 
 	@ApiOperation("Serviço para exclusão de clientes.")
 	@DeleteMapping("/api/clientes/{id}")
-	public ResponseEntity<GetClientesDTO> delete
+	public ResponseEntity<ResponseClientesDTO> delete
 (@PathVariable("id") Integer idCliente) {
-		return null;
+
+		ResponseClientesDTO response = new ResponseClientesDTO();
+
+		try {
+
+			Cliente cliente = clienteService.excluir(idCliente);
+
+			ModelMapper modelMapper = new ModelMapper();
+			
+			response.setStatus(200);
+			response.setMensagem("Cliente excluído com sucesso.");
+			response.setCliente(modelMapper.map
+(cliente, GetClientesDTO.class));
+			
+			return ResponseEntity.status(HttpStatus.OK).body(response);
+			
+		} catch (IllegalArgumentException e) {
+
+			response.setStatus(400);
+			response.setMensagem(e.getMessage());
+
+			return ResponseEntity.status
+(HttpStatus.BAD_REQUEST).body(response);
+			
+		} catch (Exception e) {
+
+			response.setStatus(500);
+			response.setMensagem(e.getMessage());
+
+			return ResponseEntity.status
+(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+		}
 	}
 
 	@ApiOperation("Serviço para consulta de clientes.")
 	@GetMapping("/api/clientes")
 	public ResponseEntity<List<GetClientesDTO>> getAll() {
-		return null;
+
+		ModelMapper modelMapper = new ModelMapper();
+		
+		List<Cliente> clientes = clienteService.consultarTodos();
+		List<GetClientesDTO> clientesDTO = modelMapper.map
+(clientes, new TypeToken<List<GetClientesDTO>>() {}.getType());
+		
+		return ResponseEntity.status(HttpStatus.OK).body(clientesDTO);
 	}
 
 	@ApiOperation("Serviço para consulta de cliente por id.")
 	@GetMapping("/api/clientes/{id}")
 	public ResponseEntity<GetClientesDTO> getById
 (@PathVariable("id") Integer idCliente) {
-		return null;
+
+		Cliente cliente = clienteService.obterPorId(idCliente);
+		
+		if(cliente != null) {
+			
+			ModelMapper modelMapper = new ModelMapper();
+			GetClientesDTO clienteDTO 
+= modelMapper.map(cliente, GetClientesDTO.class);
+			
+			return ResponseEntity.status(HttpStatus.OK).body(clienteDTO);
+		}
+		else {
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+		}			
 	}
 }
--- src/main/java/br/com/cotiinformatica/dtos/PostClientesDTO.java
+++ src/main/java/br/com/cotiinformatica/dtos/PostClientesDTO.java
@@ -1,5 +1,10 @@
 package br.com.cotiinformatica.dtos;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,12 +18,22 @@ import lombok.ToString;
 @ToString
 public class PostClientesDTO {
 
+	@Size(min = 6, max = 150, 
+message = "Informe de 6 a 150 caracteres.")
+	@NotBlank(message = "Por favor, informe o nome do cliente.")
 	private String nome;
-	private String cpf;
-	private String email;
-	private String telefone;
 	
+	@Pattern(regexp = "^[0-9]{11}$", 
+message = "Informe 11 dígitos numéricos.")
+	@NotBlank(message = "Por favor, informe o cpf do cliente.")
+	private String cpf;
+	
+	@Email(message = "Informe um endereço de email válido.")
+	@NotBlank(message = "Por favor, informe o email do cliente.")
+	private String email;
+	
+	@Pattern(regexp = "^[0-9]{11}", 
+message = "Informe 11 dígitos numéricos (DDD + Telefone)")
+	@NotBlank(message = "Por favor, informe o telefone do cliente.")
+	private String telefone;	
 }
-
-
-
